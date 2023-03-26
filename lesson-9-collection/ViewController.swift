376ccef1b6f6bbe1c17016e1998a2586a7d43e3f
LBkos <constantin.l.o.p@gmail.com>
2023-03-26 11:44:22 +0700

    Initial Commit

---
+++ lesson-9-collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson-9-collection
+//
+//  Created by Константин Лопаткин on 26.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
